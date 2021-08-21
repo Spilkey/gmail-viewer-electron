94cac6b78b2b219c6095976143e41a23b586f510
Spilkey <spilkey1234@gmail.com>
2021-08-21 15:04:05 -0400

    update icon

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function createWindow () {
     const win = new BrowserWindow({
-        icon:'view/gmail.png',
+        icon:path.join(__dirname, 'gmail.png'),
         titleBarStyle: "hidden",
         width: 800,
         height: 600,
