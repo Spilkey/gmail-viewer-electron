3d04c16e17c7a6c844c03debf091f584735f19da
Spilkey <spilkey1234@gmail.com>
2021-08-21 14:25:19 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        icon:'view/gmail.png',
+        titleBarStyle: "hidden",
+        width: 800,
+        height: 600,
+        webPreferences: {
+          preload: path.join(__dirname, 'view/preload.js'),
+          webviewTag: true
+        }
+    });
+    win.removeMenu();
+    win.loadFile('view/index.html');
+
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
